20e75f30b9fbeae9cc662d4cb81d64a56a15623d
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-18 11:56:04 +0800

    102-starter_and_101-complete

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -23,6 +23,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
               children: <Widget>[
                 Image.asset('assets/rollerCoaster.png'),
                 const SizedBox(height: 16.0),
-                const Text('Hello Cadets'),
+                const Text('Welcome, visitor!'),
               ],
             ),
             const SizedBox(height: 50.0),
@@ -43,6 +46,7 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Add TextField widgets (101)
             // [Name]
             TextField(
+              controller: _usernameController,
               decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Username',
@@ -53,6 +57,7 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 25.0),
             // [Password]
             TextField(
+              controller: _passwordController,
               decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Password (32 characters at max)',
@@ -63,6 +68,32 @@ class _LoginPageState extends State<LoginPage> {
             ),
 
             // TODO: Add button bar (101)
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    // TODO: Clear the text fields (101)
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                // TODO: Show the next page (101) 
+                  Navigator.pop(context);
+                  },
+                ),
+
+              ],
+            ),
+
           ],
         ),
       ),
