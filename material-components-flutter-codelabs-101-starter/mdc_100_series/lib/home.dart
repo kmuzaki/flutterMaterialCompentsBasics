d0f7d6c9d7202de6ba83aa786cc52db2fe2ee84e
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-09-20 10:58:01 +0800

    changeLayoutToAsymetric

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
@@ -14,6 +14,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'supplemental/asymmetric_view.dart';
+
 
 import 'model/product.dart';
 import 'model/products_repository.dart';
@@ -24,70 +26,70 @@ class HomePage extends StatelessWidget {
 
   // TODO: Make a collection of cards (102)
   // Replace this entire method
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
+  // List<Card> _buildGridCards(BuildContext context) {
+  //   List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-    if (products.isEmpty) {
-      return const <Card>[];
-    }
+  //   if (products.isEmpty) {
+  //     return const <Card>[];
+  //   }
 
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
+  //   final ThemeData theme = Theme.of(context);
+  //   final NumberFormat formatter = NumberFormat.simpleCurrency(
+  //       locale: Localizations.localeOf(context).toString());
 
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        // TODO: Adjust card heights (103)
-        elevation: 6.0,
+  //   return products.map((product) {
+  //     return Card(
+  //       clipBehavior: Clip.antiAlias,
+  //       // TODO: Adjust card heights (103)
+  //       elevation: 6.0,
 
-        child: Column(
-          // TODO: Center items on the card (103)
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-              // TODO: Adjust the box size (102)
-                fit: BoxFit.fitWidth
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                // TODO: Align labels to the bottom and center (103)
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                  // TODO: Change innermost Column (103)
-                  children: <Widget>[
-                  // TODO: Handle overflowing labels (103)
-                    Text(
-                      product.name,
-                      style: theme.textTheme.titleLarge,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.bodySmall,
-                    ),
-                    // End new code
-                  ],
+  //       child: Column(
+  //         // TODO: Center items on the card (103)
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: <Widget>[
+  //           AspectRatio(
+  //             aspectRatio: 18 / 11,
+  //             child: Image.asset(
+  //               product.assetName,
+  //               package: product.assetPackage,
+  //             // TODO: Adjust the box size (102)
+  //               fit: BoxFit.fitWidth
+  //             ),
+  //           ),
+  //           Expanded(
+  //             child: Padding(
+  //               padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+  //               child: Column(
+  //               // TODO: Align labels to the bottom and center (103)
+  //               mainAxisAlignment: MainAxisAlignment.end,
+  //               crossAxisAlignment: CrossAxisAlignment.center,
+  //                 // TODO: Change innermost Column (103)
+  //                 children: <Widget>[
+  //                 // TODO: Handle overflowing labels (103)
+  //                   Text(
+  //                     product.name,
+  //                     style: theme.textTheme.titleLarge,
+  //                     softWrap: false,
+  //                     overflow: TextOverflow.ellipsis,
+  //                     maxLines: 1,
+  //                   ),
+  //                   const SizedBox(height: 4.0),
+  //                   Text(
+  //                     formatter.format(product.price),
+  //                     style: theme.textTheme.bodySmall,
+  //                   ),
+  //                   // End new code
+  //                 ],
 
 
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
+  //               ),
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     );
+  //   }).toList();
+  // }
 
 
   // TODO: Add a variable for Category (104)
@@ -135,11 +137,8 @@ class HomePage extends StatelessWidget {
       ),
 
       // TODO: Add a grid view (102)
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(context) // Replace                             
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),                        
       ),
 
   
